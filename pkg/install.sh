1628cbdc43165a88243b07353705e899d0f04515
dylangmarinus-stack <dylan.g.marinus@gmail.com>
2026-05-21 21:24:58 +0200

    Create install.sh

---
+++ pkg/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Downloading latest SteamControllerGyroDSU..."
+cd /tmp
+curl -L -O https://github.com/dylangmarinus-stack/Steam-Controller-GyroDSU/releases/latest/download/SteamControllerGyroDSUSetup.zip
+echo "Extracting..."
+unzip -o SteamControllerGyroDSUSetup.zip -d /tmp
+cd /tmp/SteamControllerGyroDSUSetup
+echo "Installing..."
+bash install.sh
+rm -rf /tmp/SteamControllerGyroDSUSetup /tmp/SteamControllerGyroDSUSetup.zip
+read -n 1 -s -r -p "Finished. Press any key to exit."
+echo ""
